c5dd14a1685c8b6042c81936b2b6173d8fe79313
j0797 <anonymous@example.com>
2026-01-09 14:04:19 +0300

    feat: добавлен интерфейс PathFinder для абстракции поиска пути
    - Создан интерфейс PathFinder
    - BreadthFirstSearch теперь реализует PathFinder
    - Добавлена возможность реализации различных алгоритмов в будущем

---
+++ src/pathfinder/PathFinder.java
@@ -0,0 +1,12 @@
+package pathfinder;
+
+import entities.Entity;
+import worldmap.Coordinates;
+import worldmap.WorldMap;
+
+import java.util.List;
+
+public interface PathFinder {
+    List<Coordinates> findPath(WorldMap map, Coordinates start, Class<? extends Entity> targetType);
+}
+
